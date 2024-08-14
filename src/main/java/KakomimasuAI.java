81f868902179891b442c37b7bcd00accb6c9650e
ninja03 <69106571+ninja03@users.noreply.github.com>
2024-08-14 10:55:18 +0000

    refactoring

--- src/main/java/KakomimasuAI.java
+++ src/main/java/KakomimasuAI.java
@@ -7,154 +7,182 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
 
 public class KakomimasuAI {
 
-    public void start() throws Exception {
-        try (var client = HttpClient.newHttpClient()) {
-            var objectMapper = new ObjectMapper();
-            var textBodyHandler = HttpResponse.BodyHandlers.ofString();
-            // ゲームを作成
-            var baseUrl = "https://api.kakomimasu.com";
-            var startURL = URI.create(baseUrl + "/v1/matches/ai/players");
-            var startPayload = Map.of("guestName", "☕Javaくん", "aiName", "a1", "boardName", "A-1", "nAgent", 1);
-            var startHeaders = new String[] {"Content-Type", "application/json"};
-            var startBody = HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(startPayload));
-            var startRequest = HttpRequest.newBuilder().uri(startURL).headers(startHeaders).POST(startBody).build();
-            var startResponse = client.send(startRequest, textBodyHandler).body();
-            var game = objectMapper.readTree(startResponse);
-            var gameId = game.get("gameId").asText();
-            var pic = game.get("pic").asText();
-            
-            // 赤忍者を待つ
-            while (true) {
-                var waitStartURL = URI.create(baseUrl + "/v1/matches/" + gameId);
-                var waitStartRequest = HttpRequest.newBuilder().uri(waitStartURL).GET().build();
-                var waitStartResponse = client.send(waitStartRequest, textBodyHandler).body();
-                game = objectMapper.readTree(waitStartResponse);
-                if (game.get("startedAtUnixTime").asLong() != 0) {
+    String baseUrl = "https://api.kakomimasu.com";
+    BodyHandler<String> textBodyHandler = HttpResponse.BodyHandlers.ofString();
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    public JsonNode createGame(HttpClient client) throws Exception {
+        // ゲームを作成
+        var startURL = URI.create(baseUrl + "/v1/matches/ai/players");
+        var startPayload = Map.of("guestName", "☕Javaくん", "aiName", "a1", "boardName", "A-1", "nAgent", 1);
+        var startHeaders = new String[] {"Content-Type", "application/json"};
+        var startBody = HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(startPayload));
+        var startRequest = HttpRequest.newBuilder().uri(startURL).headers(startHeaders).POST(startBody).build();
+        var startResponse = client.send(startRequest, textBodyHandler).body();
+        return objectMapper.readTree(startResponse);
+    }
+
+    public JsonNode waitRedNinja(HttpClient client, String gameId) throws Exception {
+        // 赤忍者を待つ
+        while (true) {
+            var waitStartURL = URI.create(baseUrl + "/v1/matches/" + gameId);
+            var waitStartRequest = HttpRequest.newBuilder().uri(waitStartURL).GET().build();
+            var waitStartResponse = client.send(waitStartRequest, textBodyHandler).body();
+            var game = objectMapper.readTree(waitStartResponse);
+            if (game.get("startedAtUnixTime").asLong() != 0) {
+                return game;
+            }
+            Thread.sleep(100);
+        }
+    }
+
+    public JsonNode wait200(HttpClient client, String gameId) throws Exception{
+        while (true) {
+            var turnURL = URI.create(baseUrl + "/v1/matches/" + gameId);
+            var turnRequest = HttpRequest.newBuilder().uri(turnURL).GET().build();
+            var turnResponse = client.send(turnRequest, textBodyHandler);
+            var turnResponseBody = turnResponse.body();
+            var turnStatusCode = turnResponse.statusCode();
+            if (turnStatusCode == 200) {
+                return objectMapper.readTree(turnResponseBody);
+            }
+            Thread.sleep(100);
+        }
+    }
+
+    public State createState(JsonNode game) {
+        var agent = game.get("players").get(0).get("agents").get(0);
+        // 盤面をStateに変換する
+        var width = game.get("field").get("width").asInt();
+        var height = game.get("field").get("height").asInt();
+        var endTurn = game.get("totalTurn").asInt();
+        var state = new State(width, height, endTurn);
+        state.turn = game.get("turn").asInt();
+        state.character.x = agent.get("x").asInt();
+        state.character.y = agent.get("y").asInt();
+        var points = game.get("field").get("points");
+        var tiles = game.get("field").get("tiles");
+        state.enemyTiles = new boolean[state.h][];
+        for (var y = 0; y < state.h; y++) {
+            state.enemyTiles[y] = new boolean[state.w];
+            for (var x = 0; x < state.w; x++) {
+                var idx = state.w * y + x;
+                var tile = tiles.get(idx);
+                var type = tile.get("type").asInt();
+                var player = tile.get("player").asInt();
+                var point = points.get(idx).asInt();
+                var newPoint = 0;
+                if (!(type == 1 && player == 0)) {
+                    newPoint = point;
+                }
+                state.enemyTiles[y][x] = type == 1 && player == 1;
+                state.points[y][x] = newPoint;
+            }
+        }
+        return state;
+    }
+
+    public int[] doBeamSearch(State state) {
+        // BeamSearch
+        var bestdx = 0;
+        var bestdy = 0;
+        
+        var beamStartTime = System.currentTimeMillis();
+        if (state.character.x != -1) {
+            var beamWidth = 10000;
+            var beamDepth = 10;
+            var nowBeam = new PriorityQueue<State>(Comparator.comparingLong(s -> -s.evaluatedScore));
+            State bestState = null;
+            nowBeam.add(state);
+            for (var t = 0; t < beamDepth; t++) {
+                // System.out.println("depth: " + t);
+                var nextBeam = new PriorityQueue<State>(Comparator.comparingLong(s -> -s.evaluatedScore));
+                for (var i = 0; i < beamWidth; i++) {
+                    // System.out.print(i + " ");
+                    if (nowBeam.isEmpty()) {
+                        break;
+                    }
+                    var nowState = nowBeam.poll();
+                    var legalActions = nowState.legalActions();
+                    for (var action : legalActions) {
+                        var nextState = nowState.clone();
+                        nextState.advance(action);
+                        nextState.evaluateScore();
+                        if (t == 0) {
+                            nextState.firstAction = action;
+                        }
+                        nextBeam.add(nextState);
+                    }
+                }
+                nowBeam = new PriorityQueue<>(nextBeam);
+                bestState = nowBeam.peek();
+                if (bestState.isDone()) {
                     break;
                 }
-                Thread.sleep(100);
-            }        
+            }
+            bestdx = State.dx[bestState.firstAction];
+            bestdy = State.dy[bestState.firstAction];
+        }
+        var beamEndTime = System.currentTimeMillis();
+        System.out.println("beamTime = " + (beamEndTime - beamStartTime));
+        return new int[] {bestdx, bestdy};
+    }
+
+    public void sendActions(HttpClient client, String gameId, String pic, List<Map<String, Object>> actions) throws Exception {
+        var acitonPayload = new HashMap<>();
+        acitonPayload.put("actions", actions);
+        var actionURL = URI.create(baseUrl + "/v1/matches/" + gameId + "/actions");
+        var actionBody = HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(acitonPayload));
+        var actionHeaders = new String[] {"Content-Type", "application/json", "Authorization", pic};
+        var actionRequest = HttpRequest.newBuilder().uri(actionURL).method("PATCH", actionBody).headers(actionHeaders).build();
+        client.send(actionRequest, textBodyHandler);
+    }
+
+    public void start() throws Exception {
+        try (var client = HttpClient.newHttpClient()) {
+            var game = createGame(client);
+            var gameId = game.get("gameId").asText();
+            var pic = game.get("pic").asText();
+            game = waitRedNinja(client, gameId);
             var start = (long)game.get("startedAtUnixTime").asLong() * 1000;
             var opsec = (long)game.get("operationSec").asInt() * 1000L;
             var trsec = (long)game.get("transitionSec").asInt() * 1000L;
+            
             // 1ターン目を待つ
             var startSleepTime = Math.max(start - System.currentTimeMillis(), 0);
 			System.out.println("startSleepTime = " + startSleepTime);
             Thread.sleep(startSleepTime);
+
             // メインループ
             while (true) {
-                while (true) {
-                    var turnURL = URI.create(baseUrl + "/v1/matches/" + gameId);
-                    var turnRequest = HttpRequest.newBuilder().uri(turnURL).GET().build();
-                    var turnResponse = client.send(turnRequest, textBodyHandler);
-					var turnResponseBody = turnResponse.body();
-					var turnStatusCode = turnResponse.statusCode();
-                    if (turnStatusCode == 200) {
-                        game = objectMapper.readTree(turnResponseBody);
-                        break;
-                    }
-                    Thread.sleep(100);
-                }
+                game = wait200(client, gameId);
                 var status = game.get("status").asText();
                 if (status.equals("ended")) {
                     break;
                 }
-                var agent = game.get("players").get(0).get("agents").get(0);
-
-                // 盤面をStateに変換する
-                var width = game.get("field").get("width").asInt();
-                var height = game.get("field").get("height").asInt();
-                var endTurn = game.get("totalTurn").asInt();
-                var state = new State(width, height, endTurn);
-                state.turn = game.get("turn").asInt();
-                state.character.x = agent.get("x").asInt();
-                state.character.y = agent.get("y").asInt();
-                var points = game.get("field").get("points");
-                var tiles = game.get("field").get("tiles");
-                var enemyTiles = new boolean[state.h][];
-                for (var y = 0; y < state.h; y++) {
-                    enemyTiles[y] = new boolean[state.w];
-                    for (var x = 0; x < state.w; x++) {
-                        var idx = state.w * y + x;
-                        var tile = tiles.get(idx);
-                        var type = tile.get("type").asInt();
-                        var player = tile.get("player").asInt();
-                        var point = points.get(idx).asInt();
-                        var newPoint = 0;
-                        if (!(type == 1 && player == 0)) {
-                            newPoint = point;
-                        }
-                        enemyTiles[y][x] = type == 1 && player == 1;
-                        state.points[y][x] = newPoint;
-                    }
-                }
+                var state = createState(game);
                 state.print();
 
-                // BeamSearch
-                var bestdx = 0;
-                var bestdy = 0;
-                
-                var beamStartTime = System.currentTimeMillis();
-                if (state.character.x != -1) {
-                    var beamWidth = 10000;
-                    var beamDepth = 10;
-                    var nowBeam = new PriorityQueue<State>(Comparator.comparingLong(s -> -s.evaluatedScore));
-                    State bestState = null;
-                    nowBeam.add(state);
-                    for (var t = 0; t < beamDepth; t++) {
-                        // System.out.println("depth: " + t);
-                        var nextBeam = new PriorityQueue<State>(Comparator.comparingLong(s -> -s.evaluatedScore));
-                        for (var i = 0; i < beamWidth; i++) {
-                            // System.out.print(i + " ");
-                            if (nowBeam.isEmpty()) {
-                                break;
-                            }
-                            var nowState = nowBeam.poll();
-                            var legalActions = nowState.legalActions();
-                            for (var action : legalActions) {
-                                var nextState = nowState.clone();
-                                nextState.advance(action);
-                                nextState.evaluateScore();
-                                if (t == 0) {
-                                    nextState.firstAction = action;
-                                }
-                                nextBeam.add(nextState);
-                            }
-                        }
-                        nowBeam = new PriorityQueue<>(nextBeam);
-                        bestState = nowBeam.peek();
-                        if (bestState.isDone()) {
-                            break;
-                        }
-                    }
-                    bestdx = State.dx[bestState.firstAction];
-                    bestdy = State.dy[bestState.firstAction];
-                }
-                var beamEndTime = System.currentTimeMillis();
-                System.out.println("beamTime = " + (beamEndTime - beamStartTime));
-
+                // 忍者の動きを決定
+                var bestAction = doBeamSearch(state);
+ 
                 // 忍者の動きの送信
-                var acitonPayload = new HashMap<>();
                 Map<String, Object> action1 = null;
 				if (state.character.x == -1) {
 					action1 = Map.of("agentId", 0, "type", "PUT", "x", 4, "y", 4);
 				} else {
-					var nx = state.character.x + bestdx;
-					var ny = state.character.y + bestdy;
-                    var type = enemyTiles[ny][nx] ? "REMOVE" : "MOVE";
+					var nx = state.character.x + bestAction[0];
+					var ny = state.character.y + bestAction[1];
+                    var type = state.enemyTiles[ny][nx] ? "REMOVE" : "MOVE";
 					action1 = Map.of("agentId", 0, "type", type, "x", nx, "y", ny);
 				}
 				if (action1 != null) {
 					var actions = List.of(action1);
-					acitonPayload.put("actions", actions);
-					var actionURL = URI.create(baseUrl + "/v1/matches/" + gameId + "/actions");
-					var actionBody = HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(acitonPayload));
-					var actionHeaders = new String[] {"Content-Type", "application/json", "Authorization", pic};
-					var actionRequest = HttpRequest.newBuilder().uri(actionURL).method("PATCH", actionBody).headers(actionHeaders).build();
-					client.send(actionRequest, textBodyHandler);
+                    sendActions(client, gameId, pic, actions);
 				}
                 var sleepTime = Math.max(start + (long) (opsec + trsec) * state.turn - System.currentTimeMillis(), 0);
                 Thread.sleep(sleepTime);
@@ -194,6 +222,7 @@ class State implements Cloneable {
     int gameScore = 0;
     long evaluatedScore = 0;
     int firstAction = -1;
+    boolean[][] enemyTiles;
 
     State(int h, int w, int endTurn) {
         this.h = h;
